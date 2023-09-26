c091bd23d763b7159861a2a5b3f68dec4c9ae004
Omotayo <kehindeomotayo11@gmail.com>
2023-09-26 12:08:23 +0100

    Done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f'Hello, {name}!')
+
+greet('programmer')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+sum = add(45, 55)
+print(sum)
+
 
 def halve(number):
-    pass
+    if type(number) not in (int, float):
+        return None
+
+    return number / 2
+
+halve = halve('99')
+print(halve)
